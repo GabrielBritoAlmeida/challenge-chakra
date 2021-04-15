d8b7f0ba0b41f00196a75fe093689df41ab78b81
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-15 16:14:33 -0300

    feat: component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import { Flex, Image } from '@chakra-ui/react'
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Flex w="100vw" h="100px" align="center" justify="center">
+      <Image
+        boxSize="100%"
+        objectFit="cover"
+        src="/images/logo.svg"
+        alt="Logo com texto world trip"
+      />
+    </Flex>
   )
 }
