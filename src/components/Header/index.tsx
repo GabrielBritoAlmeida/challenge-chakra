f6f657ddc149e3480c50ebfd06d9b0e1bf188b60
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-18 20:23:50 -0300

    feat: page continents

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export function Header() {
   const { asPath } = useRouter()
@@ -16,15 +17,18 @@ export function Header() {
       position="relative"
     >
       {!isHome && (
-        <Image
-          position="absolute"
-          top={['46%', '36%']}
-          left="10%"
-          boxSize={['16px', '26px']}
-          objectFit="cover"
-          src="/images/arrowLeft.svg"
-          alt="Logo com texto world trip"
-        />
+        <Link href="/" passHref>
+          <Image
+            cursor="pointer"
+            position="absolute"
+            top={['46%', '36%']}
+            left="10%"
+            boxSize={['16px', '26px']}
+            objectFit="cover"
+            src="/images/arrowLeft.svg"
+            alt="Logo com texto world trip"
+          />
+        </Link>
       )}
 
       <Image
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,7 @@
+export default function Continent() {
+  return (
+    <div>
+      <h1>Continente</h1>
+    </div>
+  )
+}
